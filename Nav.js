58b059634dfac5e4fec4074dc2743ddebd4d97ec
sameer <sameer99899@gmail.com>
2025-02-13 14:28:07 +0530

    sameer

---
+++ Nav.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+
+function Nav() {
+  return (
+    <>
+    <h1>Home</h1>
+    <h1>About</h1>
+    <h1>Contact</h1>
+
+    </>
+  );
+}
+
+export default Nav;
